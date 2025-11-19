3ffbb5338b01ed94e0ddd33c460caaa1490c545c
Finn Roberts <finn.a.roberts@gmail.com>
2025-11-19 10:36:20 +0100

    improve temporal alignment resolution errors

--- tests/testthat/test-align_move.R
+++ tests/testthat/test-align_move.R
@@ -25,11 +25,43 @@ test_that("align_move (default)", {
   
   # false calls
   expect_error(align_move(NA, verbose = F)) # wrong class
-  expect_error(align_move(m, res = FALSE, verbose = F))
-  expect_error(align_move(m, start_end_time = T, verbose = F))
-  expect_error(expect_warning(align_move(m, res = 1, unit = "days", verbose = F)))
   
   # warnings
   expect_warning(align_move(m, digit = "max", verbose = F))
 })
 #}
+
+test_that("informative errors on bad temporal resolution inputs", {
+  expect_error(align_move(m, res = FALSE, verbose = F))
+  expect_error(align_move(m, start_end_time = T, verbose = F))
+  expect_error(expect_warning(align_move(m, res = 1, unit = "days", verbose = F)))
+  
+  expect_error(
+    align_move(m, res = units::set_units(20, "hours"), verbose = FALSE),
+    "exceeds the temporal range used for alignment"
+  )
+  expect_error(
+    align_move(
+      m, 
+      res = units::set_units(5, "hours"), 
+      start_end_time = c(
+        as.POSIXct("2018-05-15 08:00:00", "UTC"), 
+        as.POSIXct("2018-05-15 12:00:00", "UTC")
+      ),
+      verbose = FALSE
+    ),
+    "exceeds the temporal range used for alignment"
+  )
+  expect_error(
+    align_move(m, res = units::set_units(8, "hours"), verbose = FALSE),
+    "too coarse for some tracks: \"T246a\", \"T342g\", \"T932u\""
+  )
+  expect_error(
+    align_move(m, res = units::set_units(6, "hours"), verbose = FALSE),
+    "too coarse for some tracks: \"T342g\", \"T932u\""
+  )
+  expect_warning(
+    align_move(m, res = units::set_units(5, "hours"), verbose = FALSE),
+    "only two positions for at least one track"
+  )
+})
